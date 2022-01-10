484e2a8d86ee5f3317e437cee9098d9cba1ca16e
Hamish Henare <hamishjustin7@gmail.com>
2022-01-10 14:00:02 +1300

    created navbar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,78 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar,
-  NavbarBrand,
-  NavbarToggler,
-  Collapse,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-   } from 'reactstrap';
+import NavbarComponent from './components/NavbarComponent';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar
-    color="dark"
-    dark
-    expand="md"
-    fixed=""
-  >
-    <NavbarBrand href="/">
-      Hamish's sick gym app
-    </NavbarBrand>
-    <NavbarToggler onClick={function noRefCheck(){}} />
-    <Collapse navbar>
-      <Nav
-        className="me-auto"
-        navbar
-      >
-        <NavItem>
-          <NavLink href="/components/">
-            Components
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="https://github.com/reactstrap/reactstrap">
-            GitHub
-          </NavLink>
-        </NavItem>
-        <UncontrolledDropdown
-          inNavbar
-          nav
-        >
-          <DropdownToggle
-            caret
-            nav
-          >
-            Options
-          </DropdownToggle>
-          <DropdownMenu right>
-            <DropdownItem>
-              Option 1
-            </DropdownItem>
-            <DropdownItem>
-              Option 2
-            </DropdownItem>
-            <DropdownItem divider />
-            <DropdownItem>
-              Reset
-            </DropdownItem>
-          </DropdownMenu>
-        </UncontrolledDropdown>
-      </Nav>
-      <NavbarText>
-        The app to track my lifts instead of spreadsheets
-      </NavbarText>
-    </Collapse>
-  </Navbar>
+     <NavbarComponent />
     </div>
   );
 }

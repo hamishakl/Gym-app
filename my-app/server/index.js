336b4d2f01dd7555a1b99d2578879d243838a676
Hamish Henare <hamishjustin7@gmail.com>
2022-01-12 11:36:24 +1300

    setting up heroku

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -1,4 +1,5 @@
 // server/index.js
+const path = require('path');
 
 const express = require("express");
 
@@ -6,10 +7,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({ message: "Dynamo is the best" });
-  });
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+// Handle GET requests to /api route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
